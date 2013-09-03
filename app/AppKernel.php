66713df34b6b357b2556cb1f1caae5bfb9e763b6
unknown <Adiego.Soto@DSOTOJ-PC.cademsmart.cl>
2013-09-03 17:02:38 -0400

    Se agregó FOS-JS-Routing bundle Se cargan algunas tablas con datatable desde el base auditores y estudios Hay algunos problemas con el estilo de las tablas

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
 			new FOS\UserBundle\FOSUserBundle(),
             new Cadem\AdminBundle\CademAdminBundle(),
+			new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {            
